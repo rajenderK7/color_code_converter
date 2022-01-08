74ad21bfbf066d5ab3849dceb222c934a4a00140
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-01-08 21:34:29 +0530

    commit

--- lib/components/main_body.dart
+++ lib/components/main_body.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
-
 import 'package:color_code_converter/components/color_quote.dart';
 import 'package:color_code_converter/components/converter_ui.dart';
+import 'package:color_code_converter/components/mobile_view.dart';
 import 'package:flutter/material.dart';
 
 class MainBody extends StatelessWidget {
@@ -10,62 +10,84 @@ class MainBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
-    return Container(
-      height: 600,
-      width: double.infinity,
-      // color: Colors.green,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage(
-                "assets/images/fakurian-design-E8Ufcyxz514-unsplash.jpg"),
-            fit: BoxFit.cover),
-      ),
-      child: Center(
-        child: ClipRRect(
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 6,
-              sigmaY: 6,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 768) {
+          return const MobileView();
+        } else {
+          return Container(
+            height: 600,
+            width: double.infinity,
+            // color: Colors.green,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                      "assets/images/fakurian-design-E8Ufcyxz514-unsplash.jpg"),
+                  fit: BoxFit.cover),
             ),
-            child: Container(
-              width: _size.width * 0.8,
-              height: 500,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.white.withOpacity(0.2),
-                    Colors.white.withOpacity(0.05)
+            child: Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 30,
+                      spreadRadius: 20,
+                      color: Colors.black.withOpacity(0.1),
+                    ),
                   ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
                 ),
-              ),
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: const [
-                            ColorQuote(),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            ConverterUI(),
+                child: ClipRRect(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 6,
+                      sigmaY: 6,
+                    ),
+                    child: Container(
+                      width: _size.width * 0.8,
+                      height: 500,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: Colors.black.withOpacity(0.2),
+                        ),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.white.withOpacity(0.3),
+                            Colors.white.withOpacity(0.08)
                           ],
-                        )
-                      ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  children: const [
+                                    ColorQuote(),
+                                    SizedBox(
+                                      width: 20,
+                                    ),
+                                    ConverterUI(),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
