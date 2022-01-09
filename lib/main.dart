bae8603201e8189f74c587357e64b312c2126181
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-01-09 12:54:44 +0530

    removed gesture dectector

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusScopeNode currentFocus = FocusScope.of(context);
-
-        if (!currentFocus.hasPrimaryFocus) {
-          currentFocus.unfocus();
-        }
-      },
-      child: const MaterialApp(
-        title: 'Color Code++',
-        debugShowCheckedModeBanner: false,
-        home: HomePage(),
-      ),
+    return const MaterialApp(
+      title: 'Color Code++',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
